82c8018d3ab3a0e77246a555194e3d23737989e3
jimin-oh <wlals6828@daum.net>
2021-04-06 09:09:58 +0900

    server start

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(cors())
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+
+const con = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    database : conf.database,
+    port :  conf.port
+
+});
+
+con.connect()
+    
+app.listen(port ,  ()=> console.log(`Listening on port ${port}`));

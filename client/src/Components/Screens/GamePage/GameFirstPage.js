c1385a29bb1f895214d5c0fbd7049e69ebdcded5
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-04-20 01:57:46 +0900

    rename, move files

---
+++ client/src/Components/Screens/GamePage/GameFirstPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Game() {
+  return (
+    <div>
+      {/* 사진 보여준 다음, 3초 뒤에 GameEnd */}
+      Game 첫 화면
+    </div>
+  )
+}
+
+export default Game
---
+++ client/src/Components/Screens/GamePage/GameLandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function GameEnd() {
+  return (
+    <div>
+      {/*왼쪽에 웹캠, 오른쪽에 다음 포즈 사진 */}
+      Game 두번째 화면
+    </div>
+  )
+}
+
+export default GameEnd

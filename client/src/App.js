6b50414727e221b32e68544af2307350f2be098a
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-04-20 10:30:10 +0900

    install react-router-dom, router setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import MainPage from './Components/Screens/MainPage/MainPage';
+import HowToUsePage from './Components/Screens/HowToUsePage/HowToUsePage';
+import GameFirstPage from './Components/Screens/GamePage/GameFirstPage';
+import GameLandingPage from './Components/Screens/GamePage/GameLandingPage';
+import RankPage from './Components/Screens/RankPage/RankPage';
 
-import Home from './Components/Screens/Home';
-import Game from './Components/Screens/Game';
-import Use from './Components/Screens/Use';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 function App() {  
   return (
-    <Home />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ MainPage }></Route>
+        <Route exact path="/how-to-use" component={ HowToUsePage }></Route>
+        <Route exact path="/game-start" component={ GameFirstPage }></Route>
+        <Route exact path="/game-landing" component={ GameLandingPage }></Route>
+        <Route exact path="/rank" component={ RankPage }></Route>
+      </Switch>
+    </Router>
+    
   );
 }
 
